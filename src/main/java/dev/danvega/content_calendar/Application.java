f4f2b871d0d75137ace44f510b05b80506b7568f
hi-jennie <jennie.wang2024@gmail.com>
2024-10-15 16:50:22 +0800

    using dataLoader to initialize data

--- src/main/java/dev/danvega/content_calendar/Application.java
+++ src/main/java/dev/danvega/content_calendar/Application.java
@@ -1,17 +1,9 @@
 package dev.danvega.content_calendar;
 
 import dev.danvega.content_calendar.config.ContentCalendarProperties;
-import dev.danvega.content_calendar.model.Content;
-import dev.danvega.content_calendar.model.Status;
-import dev.danvega.content_calendar.model.Type;
-import dev.danvega.content_calendar.repository.ContentRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-
-import java.time.LocalDateTime;
 
 @EnableConfigurationProperties(ContentCalendarProperties.class)
 @SpringBootApplication
@@ -29,21 +21,22 @@ public class Application {
     }
 
     // 使用这样的方式来初始化数据（删掉了schema.sql中Insert Data的部分）
-    @Bean
-    CommandLineRunner runner(ContentRepository repository) {
-        return args -> {
-            Content content = new Content(null,
-                    "title",
-                    "description",
-                    Status.IDEA,
-                    Type.VIDEO,
-                    LocalDateTime.now(),
-                    LocalDateTime.now().plusMinutes(1),
-                    "http://www.baidu.com");
-            repository.save(content);
-            System.out.println("hello, world1");
-        };
-    }
+    // 注释掉这部分是因为改用DataLoader和.json文件来初始化数据
+//    @Bean
+//    CommandLineRunner runner(ContentRepository repository) {
+//        return args -> {
+//            Content content = new Content(null,
+//                    "title",
+//                    "description",
+//                    Status.IDEA,
+//                    Type.VIDEO,
+//                    LocalDateTime.now(),
+//                    LocalDateTime.now().plusMinutes(1),
+//                    "http://www.baidu.com");
+//            repository.save(content);
+//            System.out.println("hello, world1");
+//        };
+//    }
     // 为什么没有调用runner()方法？还是执行了CommandLineRunner的run()方法？
     // 当 Spring Boot 应用启动时，Spring 会自动扫描所有实现了 CommandLineRunner 接口的 Bean 并调用它们的 run() 方法。
     // 这也是为什么DataLoader类仍然会被 Spring 扫描到并执行的原因。
--- src/main/java/dev/danvega/content_calendar/config/DataLoader.java
+++ src/main/java/dev/danvega/content_calendar/config/DataLoader.java
@@ -1,16 +1,35 @@
 package dev.danvega.content_calendar.config;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.danvega.content_calendar.model.Content;
+import dev.danvega.content_calendar.repository.ContentRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
+
+import java.io.InputStream;
+import java.util.List;
 
 // this represents that this class will only be loaded when the profile is not dev
 @Profile("!dev")
 @Component
 public class DataLoader implements CommandLineRunner {
+    private final ContentRepository repository;
+    private final ObjectMapper objectMapper;
+
+    public DataLoader(ContentRepository repository, ObjectMapper objectMapper) {
+        this.repository = repository;
+        this.objectMapper = objectMapper;
+    }
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("hello, world2");
+        try (InputStream inputStream = TypeReference.class.getResourceAsStream("/data/content.json")) {
+            repository.saveAll(objectMapper.readValue(inputStream, new TypeReference<List<Content>>() {
+            }));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
